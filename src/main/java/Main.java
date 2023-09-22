5a3bbd23f6efc9e71ce186f893b2c860dd8def49
migue <migue@GODINEZ>
2023-09-22 15:29:23 -0600

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+/*
+
+ITESS/TICS
+TALLER DE INVESTIGACIO 1
+INTENTO DE CURSO DE CERTIFICACIO JAVA
+BY MIGUEL GODINEZ QUINTANA
+22/SEPTIMBRE/2023
+
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author miguel
+ */
+public class Main {
+    public static void main(String ... args) {
+        
+        
+    }
+}
